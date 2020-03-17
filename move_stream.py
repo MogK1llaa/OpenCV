9d2784a566af5a5699a1c7d1a1270ddcd4da38f9
MogK1llaa <mog1997@gmail.com>
2020-03-17 00:17:07 +0000

    deletes

--- move_stream.py
+++ move_stream.py
@@ -4,40 +4,35 @@ import numpy as np
 
 # Capturing video from webcam:
 cap = cv2.VideoCapture(0)
-cap1 = cv2.VideoCapture(1)
 
 currentFrame = 0
 while(True):
     # Capture frame-by-frame
     ret, frame1 = cap.read()
     ret, frame2 = cap.read()
-    ret, frame11 = cap1.read()
-    ret, frame22 = cap1.read()
 
     # Handles the mirroring of the current frame
     frame1 = cv2.flip(frame1,1)
     frame2 = cv2.flip(frame2,1)
-    frame11 = cv2.flip(frame11,1)
-    frame22 = cv2.flip(frame22,1)
+
 
     diff = cv2.absdiff(frame1,frame2)
-    diff1 = cv2.absdiff(frame11,frame22)
+ 
     
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
-    gray1 = cv2.cvtColor(diff1, cv2.COLOR_BGR2GRAY)
+
    
     blur = cv2.GaussianBlur(gray, (5,5), 0)
-    blur1 = cv2.GaussianBlur(gray1, (5,5), 0)
+
    
     _, thresh = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
-    _, thresh1 = cv2.threshold(blur1, 20, 255, cv2.THRESH_BINARY)
+   
     
     dilated = cv2.dilate(thresh, None, iterations=3)
-    dilated1 = cv2.dilate(thresh1, None, iterations=3)
+   
     
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    contours1, _ = cv2.findContours(dilated1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+    
     for contour in contours:
         (x,y,w,h)= cv2.boundingRect(contour)
 
@@ -64,12 +59,6 @@ while(True):
     frame1 = frame2
     ret, frame2 = cap.read()
 
-
-    cv2.namedWindow('camera', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('camera', 640,480)
-    cv2.imshow('camera',frame11)
-    frame11 = frame22
-    ret, frame22 = cap1.read()
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
